7b6c2b343d32e8170d4ebdfaae123c7f8f37b8ef
shivimishra <shivimishra130103@gmail.com>
2024-03-24 02:56:31 +0530

    final commit

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,43 @@
-import express from 'express';
+import express from "express";
 import http from "http";
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
-import { Server } from 'socket.io'
+import { Server } from "socket.io";
 
 const io = new Server(server, {
-    cors: {
-        origin: '*', //allowing access to anyone, Not good for production
-    },
-})
+  cors: {
+    origin: "*", //allowing access to anyone, Not good for production
+  },
+});
 
-type Point = {x: number, y: number}
+type Point = { x: number; y: number };
 
 type DrawLine = {
-    prevPoint: Point | null
-    currentPoint: Point
-    color: string
-}
+  prevPoint: Point | null;
+  currentPoint: Point;
+  color: string;
+};
 
-io.on('connection', (socket) => {
-    socket.on('draw-line', ({prevPoint, currentPoint, color}: DrawLine) => {
-        socket.broadcast.emit('draw-line', { prevPoint, currentPoint, color })
-    })
-})
+io.on("connection", (socket) => {
+  console.log("connection");
+
+  socket.on("client-ready", () => {
+    socket.broadcast.emit("get-canvas-state");
+  });
+
+  socket.on("canvas-state", (state) => {
+    socket.broadcast.emit("canvas-state-from-server", state);
+  });
+
+  socket.on("draw-line", ({ prevPoint, currentPoint, color }: DrawLine) => {
+    socket.broadcast.emit("draw-line", { prevPoint, currentPoint, color }); //not emitted to the sender
+  });
+
+  socket.on("clear", () => io.emit("clear")); // io.emit will emit it to everyone(including the sender)
+});
+
+server.listen(3001, () => {
+  console.log("Server listening on port 3001");
+});
